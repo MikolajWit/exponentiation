d6f656d930b37e4ddc803e36447e3fb42389cb5f
Mikolaj <mikoort@gmail.com>
2020-07-20 22:22:59 +0200

    test

--- exponentiation_func.py
+++ exponentiation_func.py
@@ -1,1 +1,27 @@
+def exponentiation(base, exponent):
+    if exponent == 0:
+        return 1
+    if base == 0 and exponent < 0:
+        raise ZeroDivisionError
+    if base == 0:
+        return 0
+
+    if base > 0 and exponent > 0:
+        result = base
+        for n in range(exponent - 1 ):
+            result *= base
+        return result
+
+    if base < 0 and exponent < 0:
+        result = 1/base
+        for n in range(-exponent - 1):
+            result /= base
+        return result
+
+    if base < 0 and exponent > 0:
+        result = base
+        for n in range(exponent - 1):
+            result *= base
+        return result
+
 
--- exponentiation_test.py
+++ exponentiation_test.py
@@ -1,1 +1,31 @@
+import unittest
+from exponentiation_func import exponentiation
+
+class ExponentiationTest(unittest.TestCase):
+
+    def test_exponentiation_with_positive_base_and_exponent(self):
+        self.assertEqual(exponentiation(2, 2), 4)
+        self.assertEqual(exponentiation(5, 3), 125)
+        self.assertEqual(exponentiation(2, 3), 8)
+
+    def test_exponentiation_with_negative_base_and_exponent(self):
+
+        self.assertEqual(exponentiation(-2, 3), -8)
+        self.assertEqual(exponentiation(-2, -3), -0.125)
+
+
+    def test_exponentiation_with_base_as_zero(self):
+        self.assertEqual(exponentiation(0, 1), 0)
+        self.assertEqual(exponentiation(0, 100), 0)
+
+    def test_exponentiation_with_exponent_as_zero(self):
+        self.assertEqual(exponentiation(1, 0), 1)
+        self.assertEqual(exponentiation(5, 0), 1)
+        self.assertEqual(exponentiation(9, 0), 1)
+
+    def test_exponentiation_with_base_as_zero_and_negative_exponent(self):
+        with self.assertRaises(ZeroDivisionError):
+            exponentiation(0, -3)
+
+
 
